ebfeaa898a5707507bd8cd4389f95696fcfa5aea
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-10 22:34:47 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -11,7 +11,16 @@ def toadstool
   
 end
 
+
 def link 
   phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase 
+end 
+
+def all_phrases 
+  
+  puts mario 
+  puts toadstool
+  puts link
+  
 end 
